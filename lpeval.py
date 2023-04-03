33f4b9d44a1017935957796fe4d65fd7e019fcb9
Ricky Zhu <rickyzhu@foxmail.com>
2023-04-03 15:35:55 +0000

    update

---
+++ lpeval.py
@@ -0,0 +1,19 @@
+'''
+Evaluate a KG when no gold standards available, by link prediction
+as a downstream application.
+
+:author: Ricky Zhu
+:email: ruiqi.zhu@ed.ac.uk
+'''
+import os
+
+import pykeen
+
+
+
+def eval_kg(kg):
+    '''
+    Take a KG as input, return the metrics about indicating the
+    quality of this KG.
+    '''
+    pass
